9f35926b0218218ad32005c3d6a7220ce1498279
Anna Bhore <bhore.anna0@gmail.com>
2019-08-03 16:59:16 +0530

    Update SpringBootAngularSecurityAppApplication.java
    This Changes On Commit

--- src/main/java/in/net/usit/springbootangularsecurityapp/SpringBootAngularSecurityAppApplication.java
+++ src/main/java/in/net/usit/springbootangularsecurityapp/SpringBootAngularSecurityAppApplication.java
@@ -8,7 +8,7 @@ public class SpringBootAngularSecurityAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAngularSecurityAppApplication.class, args);
-		System.out.println("Application Started.....!");
+		System.out.println("Application Started ON GITHUB AND JENKINS TESTINg .....!");
 
 	}
 
